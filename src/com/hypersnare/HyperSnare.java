1723e3c0a729a3040194bfe7aa554fc8ff19a33f
Keaton Kowal <kowalk@uw.edu>
2021-03-18 16:54:30 -0700

    Began client code

--- src/com/hypersnare/HyperSnare.java
+++ src/com/hypersnare/HyperSnare.java
@@ -3,9 +3,7 @@ package com.hypersnare;
 import com.hypersnare.dsp.Flanger;
 import com.hypersnare.dsp.Snare;
 
-import javax.sound.sampled.*;
-import javax.xml.transform.Source;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class HyperSnare {
 
@@ -14,24 +12,53 @@ public class HyperSnare {
         // randomize
         // hear
         // write
+        Scanner input = new Scanner(System.in);
+
         Snare snare = new Snare();
         Flanger flang = new Flanger();
+        double[] buffer = new double[0];
 
-        snare.ping();
-        for (int i = 0; i < StdAudio.SAMPLE_RATE; i++) {
-            StdAudio.play(flang.tick(snare.tick()));
-        }
-
-        for (int j = 0; j < 5; j++) {
-            flang.randomize();
-            snare.randomize();
-            snare.ping();
-            for (int i = 0; i < StdAudio.SAMPLE_RATE; i++) {
-                StdAudio.play(flang.tick(snare.tick()));
+        System.out.println("Hello! Welcome to HyperSnare");
+        int selection = promptAction(input);
+        while (selection != 3) {
+            if (selection == 0) {
+                buffer = fillBuffer(snare, flang);
+            } else if (selection == 1) {
+                StdAudio.play(buffer);
             }
+            selection = promptAction(input);
         }
+
 
         StdAudio.close();
         System.exit(0);
+    }
+
+    public static int promptAction(Scanner input) {
+        promptAction();
+        String choice = input.next();
+        while (!(choice.equals("0") || choice.equals("1") || choice.equals("2") || choice.equals("3"))) {
+            promptAction();
+            choice = input.next();
+        }
+        return Integer.parseInt(choice);
+    }
+
+    public static void promptAction() {
+        System.out.println("What would you like to do? (Enter a number, 0-3)");
+        System.out.println("0 - Randomize a snare");
+        System.out.println("1 - Hear the current snare");
+        System.out.println("2 - Save the current snare");
+        System.out.println("3 - Quit");
+    }
+
+    public static double[] fillBuffer(Snare snare, Flanger flanger) {
+        snare.randomize();
+        flanger.randomize();
+        double[] buffer = new double[StdAudio.SAMPLE_RATE];
+        for (int i = 0; i < buffer.length; i++) {
+            buffer[i] = flanger.tick(snare.tick());
+        }
+        return buffer;
     }
 }
--- src/com/hypersnare/dsp/Filter.java
+++ src/com/hypersnare/dsp/Filter.java
@@ -1,5 +1,7 @@
 package com.hypersnare.dsp;
 
+// TODO: Implement some sort of bandpass filter algorithm.
+
 public class Filter implements Processor {
 
     public double tick(double in) {
